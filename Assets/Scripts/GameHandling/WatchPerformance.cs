893cce72e8e867472887db99b0c2518bfa786f2c
cschob01 <carlos.schober@tufts.edu>
2026-04-08 16:17:18 -0400

    Adding A* Pathfinder Project Files. Edited a bit to make functional

--- Assets/Scripts/GameHandling/WatchPerformance.cs
+++ Assets/Scripts/GameHandling/WatchPerformance.cs
@@ -6,6 +6,7 @@ public class WatchPerformance : MonoBehaviour
     public int fps_warning_marker = 50;
     void Update()
     {
+        //Check that fps is above marker
         float fps = 1f / Time.deltaTime;
         if (fps < fps_warning_marker)
         {
