906943214498534b91bd9db9b75e72ef5c374efd
cschob01 <carlos.schober@tufts.edu>
2026-05-01 21:43:14 -0400

    Adding character models

--- Assets/Scripts/Moving Objects/MovingObject.cs
+++ Assets/Scripts/Moving Objects/MovingObject.cs
@@ -4,6 +4,7 @@ public class MovingObject : MonoBehaviour
 {
     public Vector2 vel = Vector2.zero;
     protected Rigidbody2D object_rb;
+    public GameObject render;
 
     protected virtual void Awake()
     {
@@ -14,6 +15,10 @@ public class MovingObject : MonoBehaviour
     {
         SetVel();
         object_rb.linearVelocity = vel;
+        if (render != null)
+        {
+            render.transform.rotation = Quaternion.FromToRotation(Vector2.right, vel);
+        }
     }
 
     protected virtual void SetVel() { }
